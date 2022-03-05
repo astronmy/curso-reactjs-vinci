995785cd6db6bf6c9df2ced251b32b3c6a396b16
Renzo Vinci <renzvinci@gmail.com>
2022-03-05 15:59:26 -0300

    primer componente Avatar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
+  import logo from './logo.svg';
+  import './App.css';
+ */
+import Avatar from "./components/Avatar/Avatar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +19,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Avatar />
       </header>
     </div>
   );
---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,12 @@
+import './Avatar.css'
+
+const Avatar = () => {
+  return (
+      <picture>
+          <img src="https://randomuser.me/api/portraits/lego/5.jpg" alt ="" />
+          <p>Lego 1</p>
+      </picture>
+  )
+}
+
+export default Avatar
