9603016d463bbeb55f2a6bd308f3d9efad292b88
Renzo Vinci <renzvinci@gmail.com>
2022-03-12 14:28:56 -0300

    add componentn navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+const Navbar = () =>{
+  return (
+      <nav>
+          <ul>
+              <li>Home</li>
+              <li>Products</li>
+              <li>Services</li>
+          </ul>
+      </nav>
+  )
+}
+export default Navbar
